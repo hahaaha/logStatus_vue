f6e9678dbce70c1c73b96a514e94bd1ca226757c
haha <390519764@qq.com>
2019-03-24 13:50:54 +0800

    frist version

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import qs from 'qs'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$ajax = axios
+Vue.prototype.$qs = qs
+Vue.use(ElementUI)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <h3>状态管理</h3>
+        <el-button @click="submitStatus('1')">吃饭</el-button>
+        <el-button @click="submitStatus('2')">睡觉</el-button>
+        <el-button @click="submitStatus('3')">起床</el-button>
+        <el-button @click="submitStatus('4')">上班</el-button>
+        <el-button @click="submitStatus('5')">下班</el-button>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+    methods: {
+        submitStatus: function(val) {
+            var that = this
+            this.$ajax
+                .post(
+                    "http://skill.hahahah.me/logStatus/basic/add.do",
+                    this.$qs.stringify({ status: val })
+                )
+                .then(function(response) {
+                    console.log(response);
+                    if (response.data.status == 0) {
+                        console.log(response.data.msg)
+                        that.$message({
+                            message: response.data.msg,
+                            type: 'success'
+                            })
+
+                    }
+                });
+        }
+    }
+};
 </script>
