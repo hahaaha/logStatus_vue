f9bcbb3558d418de6ad0c677cc1d77ecadc6de0b
haha <390519764@qq.com>
2019-03-25 21:42:53 +0800

    对前后端做了匹配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="home">
         <h3>状态管理</h3>
-        <el-button @click="submitStatus('1')">吃饭</el-button>
-        <el-button @click="submitStatus('2')">睡觉</el-button>
-        <el-button @click="submitStatus('3')">起床</el-button>
-        <el-button @click="submitStatus('4')">上班</el-button>
-        <el-button @click="submitStatus('5')">下班</el-button>
+        <el-button @click="submitStatus('1')">睡觉</el-button>
+        <el-button @click="submitStatus('2')">写代码</el-button>
+        <el-button @click="submitStatus('3')">吃饭</el-button>
+        <el-button @click="submitStatus('4')">起床</el-button>
+        <el-button @click="submitStatus('5')">上班</el-button>
+        <el-button @click="submitStatus('6')">下班</el-button>
     </div>
 </template>
 
